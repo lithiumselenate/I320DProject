9b031c53354efb577ed0b561674a44ef74b44a35
lithiumselenate <hankhe22@utexas.edu>
2024-04-11 23:21:43 -0500

    Changed to a runable model

--- train.py
+++ train.py
@@ -19,12 +19,41 @@ dataset = tr.MyDataset(file_path='train.parquet',
                         transforms.ToTensor(),
                         transforms.Normalize((0.5,), (0.5,))
                     ]))
-dataloader = DataLoader(dataset, batch_size=32, shuffle=True, collate_fn=tr.collate_fn) 
-''''''
+dataloader = DataLoader(dataset, batch_size=32, shuffle=True, collate_fn=tr.collate_fn)
+model = tr.CTCModel(chan_in=1,                                                             # 3 channel image - imagenet pretrained
+                 time_step=96,                                                          # this is the max length possible                                                  
+                 feature_size=512,                                                      # conv outputs 512, 32, 1
+                 hidden_size=512,                        
+                 output_size=len(dataset.char_dict),                               
+                 num_rnn_layers=4,                                                      
+                 rnn_dropout=0) 
+#model.load_pretrained_resnet()
+model.to('cuda')
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+loss_func = nn.CTCLoss(reduction='mean', zero_infinity=True)
+num_epochs = 20
+for epoch in range(num_epochs):
+    train_loader = tqdm(dataloader)
+    for images, targets in train_loader:
+        logits = model(images) 
+        logits = logits.log_softmax(2)
+        target_lengths = torch.tensor([len(t) for t in targets], dtype=torch.long)
+        T, B, H = logits.size()
+        pred_sizes = torch.LongTensor([T for i in range(B)])
+        loss = loss_func(logits, targets, pred_sizes, target_lengths)  
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        train_loader.set_description(f"Epoch {epoch+1}")
+        train_loader.set_postfix(loss=loss.item())
+
+    print(f"Epoch {epoch+1}, Loss: {loss.item()}")
+torch.save(model.state_dict(), 'model_state_dict.pth')
+'''
 crnn = tr.CRNN(opt={'imgH': 384, 'nChannels': 1, 'nHidden': 256, 'nClasses': len(tr.ALL_CHAR) + 1})
 crnn = crnn.cuda()
 optimizer = optim.Adam(crnn.parameters(), lr=0.001)
-ctc_loss = tr.CustomCTCLoss()
+loss_func = nn.CTCLoss(reduction='sum', zero_infinity=True)
 num_epochs = 20
 for epoch in range(num_epochs):
     train_loader = tqdm(dataloader)
@@ -33,11 +62,11 @@ for epoch in range(num_epochs):
         logits = logits.log_softmax(2)
         print(logits.shape) 
         print(targets.shape)
-        #target_lengths = torch.tensor([len(t) for t in targets], dtype=torch.long)
-        target_lengths = torch.tensor([t.size(0) for t in targets], dtype=torch.long)
+        target_lengths = torch.tensor([len(t) for t in targets], dtype=torch.long)
+        print(target_lengths)
         T, B, H = logits.size()
         pred_sizes = torch.LongTensor([T for i in range(B)])
-        loss = ctc_loss(logits, targets, pred_sizes, target_lengths)  
+        loss = loss_func(logits, targets, pred_sizes, target_lengths)  
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
@@ -45,4 +74,4 @@ for epoch in range(num_epochs):
         train_loader.set_postfix(loss=loss.item())
 
     print(f"Epoch {epoch+1}, Loss: {loss.item()}")
-torch.save(crnn.state_dict(), 'model_state_dict.pth')
+torch.save(crnn.state_dict(), 'model_state_dict.pth')'''
--- training_preparation.py
+++ training_preparation.py
@@ -12,7 +12,7 @@ from torch.optim.lr_scheduler import CosineAnnealingLR, StepLR
 from torch.nn.utils.clip_grad import clip_grad_norm_
 from torch.utils.data import random_split
 from tqdm import *
-ALL_CHAR = '-ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚÝàáâãèéêìíòóôõùúýĂăĐđĨĩŨũƠơƯưẠạẢảẤấẦầẨẩẪẫẬậẮắẰằẲẳẴẵẶặẸẹẺẻẼẽẾếỀềỂểỄễỆệỈỉỊịỌọỎỏỐốỒồỔổỖỗỘộỚớỜờỞởỠỡỢợỤụỦủỨứỪừỬửỮữỰựỲỳỴỵỶỷỸỹ '
+ALL_CHAR = ' -ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚÝàáâãèéêìíòóôõùúýĂăĐđĨĩŨũƠơƯưẠạẢảẤấẦầẨẩẪẫẬậẮắẰằẲẳẴẵẶặẸẹẺẻẼẽẾếỀềỂểỄễỆệỈỉỊịỌọỎỏỐốỒồỔổỖỗỘộỚớỜờỞởỠỡỢợỤụỦủỨứỪừỬửỮữỰựỲỳỴỵỶỷỸỹ'
 MAX_WIDTH= 592
 MAX_HEIGHT = 384
 def binarize_image_from_binary(data):# Convert image data to binary. Although there is such column, it would be necessry for demo or other input
@@ -42,16 +42,18 @@ class MyDataset(torch.utils.data.Dataset):
             ])
         else:
             self.transform = transform
+        self.char_dict = ALL_CHAR
+
 
     def __len__(self):
         return self.df.shape[0]
 
     def __getitem__(self, idx):
         row = self.df.iloc[idx]  
-        image_data = row['image']
+        image_data = row['binary_image']
         image = Image.open(io.BytesIO(image_data['bytes']))
-        image = image.convert('L')  
-        image = image.point(lambda x: 0 if x < 128 else 1, 'L')  
+        image = image.convert('L')
+        #image = image.point(lambda x: 0 if x < 192 else 255, 'L')  
         img_t = self.transform(image)
         return img_t, row['text']
 
@@ -161,7 +163,7 @@ class CustomCTCLoss(torch.nn.Module):
 char_to_index = {char: index for index, char in enumerate(ALL_CHAR)}
 def label_to_tensor(label):
     return torch.tensor([char_to_index[char] for char in label])
-
+import torchvision
 from torch.nn.utils.rnn import pad_sequence
 from torchvision.transforms.functional import to_tensor
 from torch.nn.functional import pad
@@ -176,10 +178,158 @@ def collate_fn(batch):
         right_pad = max_width - img.shape[2] - left_pad
         top_pad = (max_height - img.shape[1]) // 2
         bottom_pad = max_height - img.shape[1] - top_pad
-        img_padded = pad(img, (left_pad, right_pad, top_pad, bottom_pad ), "constant", 0)
+        img_padded = pad(img, (left_pad, right_pad, top_pad, bottom_pad ), "constant", 1)
         batch_images.append(img_padded)
         label_tensor = label_to_tensor(label)
         batch_labels.append(label_tensor)
     images = torch.stack(batch_images)
     batch_labels = pad_sequence(batch_labels, batch_first=True, padding_value=0) 
     return images.cuda(), batch_labels.cuda()
+from torch.utils.model_zoo import load_url
+from torchvision.models.resnet import BasicBlock
+resnet_url = 'https://download.pytorch.org/models/resnet34-333f7ec4.pth'
+def downsample(chan_in, chan_out, stride, pad=0):
+    
+    return nn.Sequential(
+            nn.Conv2d(chan_in, chan_out, kernel_size=1, stride=stride, bias=False,
+                      padding=pad),
+            nn.BatchNorm2d(chan_out)
+            )
+class CNN(nn.Module):
+    
+    def __init__(self, chan_in, time_step, zero_init_residual=False):
+        super(CNN, self).__init__()
+        
+        self.chan_in = chan_in
+        if chan_in == 3:
+            self.conv1 = nn.Conv2d(chan_in, 64, kernel_size=7, stride=2, padding=2, 
+                               bias=False)
+        else:
+            self.chan1_conv = nn.Conv2d(chan_in, 64, kernel_size=7, stride=2, padding=2, 
+                               bias=False)
+        self.relu = nn.ReLU(inplace=True)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = nn.Sequential(*[BasicBlock(64, 64) for i in range(0, 3)])
+        self.layer2 = nn.Sequential(*[BasicBlock(64, 128, stride=2, 
+                                      downsample=downsample(64, 128, 2))\
+                                      if i == 0 else BasicBlock(128, 128)\
+                                      for i in range(0, 4)])
+        self.layer3 = nn.Sequential(*[BasicBlock(128, 256, stride=(1,2),
+                                      downsample=downsample(128, 256, (1,2)))\
+                                      if i == 0 else BasicBlock(256, 256)\
+                                      for i in range(0, 6)])
+        self.layer4 = nn.Sequential(*[BasicBlock(256, 512, stride=(1,2), 
+                                      downsample=downsample(256, 512, (1,2)))\
+                                      if i == 0 else BasicBlock(512, 512)\
+                                      for i in range(0, 3)])
+        self.avgpool = nn.AdaptiveAvgPool2d(output_size=(time_step, 1))
+        
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', 
+                                        nonlinearity='relu')
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+                
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, BasicBlock):
+                    nn.init_constant_(m.bn2.weight, 0)
+                    
+    def forward(self, xb):
+        
+        if self.chan_in == 3:
+            out = self.maxpool(self.bn1(self.relu(self.conv1(xb))))
+        else:
+            out = self.maxpool(self.bn1(self.relu(self.chan1_conv(xb))))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.avgpool(out)
+        
+        return out.squeeze(dim=3).transpose(1, 2)
+    
+class RNN(nn.Module):
+    
+    def __init__(self, feature_size, hidden_size, output_size, num_layers, dropout=0):
+        super(RNN, self).__init__()
+        
+        self.lstm = nn.LSTM(input_size=feature_size, hidden_size=hidden_size, num_layers=num_layers, 
+                            bidirectional=True, batch_first=True, dropout=dropout)
+        self.atrous_conv = nn.Conv2d(hidden_size*2, output_size, kernel_size=1, dilation=1)
+        
+    def forward(self, xb):
+        out, _ = self.lstm(xb)
+        out = self.atrous_conv(out.permute(0, 2, 1).unsqueeze(3))
+        return out.squeeze(3).permute((2, 0, 1))
+        
+class CTCModel(nn.Module):
+    
+    def __init__(self, chan_in, time_step, feature_size,
+                 hidden_size, output_size, num_rnn_layers,
+                 rnn_dropout=0, zero_init_residual=False,
+                 pretrained=False, cpu=True):
+        super(CTCModel, self).__init__()
+        
+        
+        self.cnn = CNN(chan_in=chan_in, time_step=time_step, 
+                       zero_init_residual=zero_init_residual)
+        self.rnn = RNN(feature_size=feature_size, hidden_size=hidden_size, 
+                       output_size=output_size, num_layers=num_rnn_layers,
+                       dropout=rnn_dropout)
+        
+        if pretrained and cpu:
+            self.load_state_dict(torch.load('weights/iam_ctc_resnet34_weights.pth',
+                                            map_location=torch.device('cpu')))
+        elif pretrained and not cpu:
+            self.load_state_dict(torch.load('weights/iam_ctc_resnet34_weights.pth',
+                                            map_location=torch.device('cuda')))
+        
+        self.time_step = time_step
+        self.to_freeze = []
+        self.frozen = []
+    
+    def forward(self, xb):
+        xb = xb.float()
+        out = self.cnn(xb)
+        out = self.rnn(out)
+        return out
+    
+    def best_path_decode(self, xb):
+        
+        with torch.no_grad():
+            out = self.forward(xb)
+            
+            softmax_out = out.softmax(2).argmax(2).permute(1, 0).cpu().numpy()
+            char_list = []
+            for i in range(0, softmax_out.shape[0]):
+                dup_rm = softmax_out[i, :][np.insert(np.diff(softmax_out[i, :]).astype(np.bool), 0, True)]
+                dup_rm = dup_rm[dup_rm != 0]
+                char_list.append(dup_rm.astype(int))
+                
+        return char_list
+    
+    def load_pretrained_resnet(self):
+        
+        self.to_freeze = []
+        self.frozen = []
+        
+        model_dict = self.state_dict()
+        pretrained_dict = load_url(resnet_url)
+        pretrained_dict = {f'cnn.{k}': v for k, v in pretrained_dict.items() if f'cnn.{k}' in model_dict}
+        model_dict.update(pretrained_dict)
+        self.load_state_dict(pretrained_dict, strict=False)
+        for k in self.state_dict().keys():
+            if not 'running' in k and not 'track' in k:
+                self.frozen.append(False)
+                if k in pretrained_dict.keys():
+                    self.to_freeze.append(True)
+                else:
+                    self.to_freeze.append(False)
+        assert len(self.to_freeze) == len([p for p in self.parameters()])
+#class packed(object):
+#    def __init__(self, model, dataloader, )
+        

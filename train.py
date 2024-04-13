1226478adf9e373f1336fcca2944b0f474580b7f
lithiumselenate <hankhe22@utexas.edu>
2024-04-12 23:07:42 -0500

    Revert "delete not used code"
    This reverts commit 8f160014778f71f209c313f53731ba2d8b814051.

--- train.py
+++ train.py
@@ -47,3 +47,13 @@ for epoch in range(num_epochs):
 
     print(f"Epoch {epoch+1}, Loss: {loss.item()}")
 torch.save(model.state_dict(), 'model_state_dict.pth')
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+losses = np.array(losses)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(np.array(range(losses.shape[1])), np.array(range(losses.shape[0])), losses)
+plt.show()
+
+

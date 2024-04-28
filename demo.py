960015cb38e49370b66b5014cfbce1017fd85170
lithiumselenate <hankhe22@utexas.edu>
2024-04-28 02:41:42 -0500

    merge files

--- demo.py
+++ demo.py
@@ -82,3 +82,17 @@ def get_single(image):
     p = ''.join(chars)
     return p
     '''
+import gradio as gr
+import demo 
+import training_preparation
+
+inputs=gr.Image(label="Input Image", type="pil")
+output1 = gr.Textbox(label="Prediction")
+
+gr.Interface(
+    fn=demo.get_single,
+    inputs=inputs,
+    outputs=output1,
+    title="Handwriting recognition",
+    description="Select File and click on submit to get the prediction."
+).launch(share=True)
--- demo_gui.py
+++ demo_gui.py
@@ -2,13 +2,13 @@ import gradio as gr
 import demo 
 import training_preparation
 
-inputs=gr.Image(label="Input Image Component", type="pil")
-output = gr.Textbox(label="Prediction")
+inputs=gr.Image(label="Input Image", type="pil")
+output1 = gr.Textbox(label="Prediction")
 
 gr.Interface(
     fn=demo.get_single,
     inputs=inputs,
-    outputs=output,
+    outputs=output1,
     title="Handwriting recognition",
     description="Select File and click on submit to get the prediction."
 ).launch(share=True)
